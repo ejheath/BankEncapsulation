792e9819408f1a7c84ee91e79f718b3790eb187e
Elijah Heath <ejheath8888@gmail.com>
2022-02-22 11:51:10 -0500

    encapsulation with bank accounts

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine("How much do you want to deposit?");
+            var deposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(deposit);
+
+            Console.WriteLine($"Your balance is ${account.GetBalance()}");
+        }
+    }
+}
